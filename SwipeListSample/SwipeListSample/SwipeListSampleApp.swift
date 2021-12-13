dda62aaa4e9573ed5e0f20111babab08e12951fa
LeoAndo <leo.777.info@gmail.com>
2021-12-13 23:32:01 +0900

    add project..

---
+++ SwipeListSample/SwipeListSample/SwipeListSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeListSampleApp.swift
+//  SwipeListSample
+//
+//  Created by LeoAndo on 2021/12/13.
+//
+
+import SwiftUI
+
+@main
+struct SwipeListSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
