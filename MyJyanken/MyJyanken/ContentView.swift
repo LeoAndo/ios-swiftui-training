025b1b425567e59587153f8f9401d4036de6d673
LeoAndo <reo.ando@sun-asterisk.com>
2022-04-25 00:54:03 +0900

    Spacerを追加

--- MyJyanken/MyJyanken/ContentView.swift
+++ MyJyanken/MyJyanken/ContentView.swift
@@ -11,24 +11,32 @@ struct ContentView: View {
     @State var answerNumber = 0
     var body: some View {
         VStack {
+            Spacer()
             if answerNumber == 0 {
                 // 初期状態
                 Text("これからジャンケンをします")
+                    .padding(.bottom)
             } else if answerNumber == 1 {
                 Image("gu")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 Text("グー")
+                    .padding(.bottom)
+                Spacer()
             } else if answerNumber == 2 {
                 Image("choki")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 Text("チョキ")
+                    .padding(.bottom)
+                Spacer()
             } else {
                 Image("pa")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 Text("パー")
+                    .padding(.bottom)
+                Spacer()
             }
             Button(action: {
                 var randomNumber = 0
@@ -40,6 +48,11 @@ struct ContentView: View {
                 answerNumber = randomNumber
             }) {
                 Text("ジャンケンをする")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(Color.pink)
+                    .foregroundColor(Color.white)
             }
         }
         
