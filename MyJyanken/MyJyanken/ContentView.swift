7a22d12ace2a83e0a55737bd05b2d77ef45aff5c
LeoAndo <reo.ando@sun-asterisk.com>
2022-04-25 00:01:57 +0900

    タップするとジャンケンの手の画像を切り替える

--- MyJyanken/MyJyanken/ContentView.swift
+++ MyJyanken/MyJyanken/ContentView.swift
@@ -8,19 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var answerNumber = 0
     var body: some View {
         VStack {
-            Image("gu")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            Text("グー")
+            if answerNumber == 0 {
+                // 初期状態
+                Text("これからジャンケンをします")
+            } else if answerNumber == 1 {
+                Image("gu")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("グー")
+            } else if answerNumber == 2 {
+                Image("choki")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("チョキ")
+            } else {
+                Image("pa")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("パー")
+            }
             Button(action: {
                 print("クリックされました")
+                answerNumber = answerNumber + 1
             }) {
                 Text("ジャンケンをする")
             }
         }
-
+        
     }
 }
 
