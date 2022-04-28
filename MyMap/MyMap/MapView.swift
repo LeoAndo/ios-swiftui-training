c0caebb1e0f44fb8399a058550460efdd47485e8
LeoAndo <reo.ando@sun-asterisk.com>
2022-04-28 17:00:45 +0900

    add template project.

---
+++ MyMap/MyMap/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  MyMap
+//
+//  Created by LeoAndo on 2022/04/28.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
