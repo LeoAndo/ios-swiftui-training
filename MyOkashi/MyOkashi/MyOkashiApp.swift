639f2d51d7b22384f6efa24c3d7787a81e83da21
LeoAndo <reo.ando@sun-asterisk.com>
2022-04-30 23:27:09 +0900

    add project.

---
+++ MyOkashi/MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by LeoAndo on 2022/04/30.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
