57003a0cd316d69b3e17d68a3e024cc97a403719
LeoAndo <reo.ando@sun-asterisk.com>
2022-05-01 01:03:10 +0900

    Safariを起動する

--- MyOkashi/MyOkashi/ContentView.swift
+++ MyOkashi/MyOkashi/ContentView.swift
@@ -13,29 +13,48 @@ struct ContentView: View {
     @StateObject var okashiDataList = OkashiData()
     // 入力された文字列を保持する状態変数
     @State var inputText = ""
+    // SafariViewの表示有無を管理する変数
+    @State var showSafari = false
     
     var body: some View {
         VStack {
             TextField("キーワード", text: $inputText, prompt: Text("キーワードを入力してください"))
                 .onSubmit {
-                    Task { await okashiDataList.searchOkashi(keyword: inputText) }
+                    Task {
+                        await okashiDataList.searchOkashi(keyword: inputText)
+                        
+                    }
                 }
-                .submitLabel(.search) // キーボードの改行を検索に変更する
+                .submitLabel(.search)  // キーボードの改行を検索に変更する
                 .padding()
             
+            // リスト表示する
             List(okashiDataList.okashiList) { okashi in
-                HStack {
-                    AsyncImage(url: okashi.image) { image in
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 40)
-                    } placeholder: {
-                        ProgressView() // 読み込み中はインジケーターを表示する
+                Button(action: {
+                    showSafari.toggle()
+                }) {
+                    // Listの表示内容を生成する
+                    HStack {
+                        AsyncImage(url: okashi.image) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 40)
+                            
+                        } placeholder: {
+                            // 読み込み中はインジケーターを表示する
+                            ProgressView()
+                        }
+                        Text(okashi.name)
                     }
-                    Text(okashi.name)
+                    
                 }
+                .sheet(isPresented: $showSafari, content: {
+                    SafariView(url: okashi.link)
+                        .edgesIgnoringSafeArea(.bottom) // 画面下部がセーフエリア外までいっぱいなるように指定
+                })
             }
+            
         }
     }
 }
