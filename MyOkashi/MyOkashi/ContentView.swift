6e1071daf4e241b93d645e5ba4d2ac662dd23565
LeoAndo <reo.ando@sun-asterisk.com>
2022-04-30 23:55:22 +0900

    画面作成とAPI周りの実装

--- MyOkashi/MyOkashi/ContentView.swift
+++ MyOkashi/MyOkashi/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // OkashiDataを参照する状態変数
+    @StateObject var okashiDataList = OkashiData()
+    // 入力された文字列を保持する状態変数
+    @State var inputText = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("キーワード", text: $inputText, prompt: Text("キーワードを入力してください"))
+                .onSubmit {
+                    Task { await okashiDataList.searchOkashi(keyword: inputText) }
+                }
+                .submitLabel(.search) // キーボードの改行を検索に変更する
+                .padding()
+            
+            List(okashiDataList.okashiList) { okashi in
+                HStack {
+                    AsyncImage(url: okashi.image) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 40)
+                    } placeholder: {
+                        ProgressView() // 読み込み中はインジケーターを表示する
+                    }
+                    Text(okashi.name)
+                }
+            }
+        }
     }
 }
 
