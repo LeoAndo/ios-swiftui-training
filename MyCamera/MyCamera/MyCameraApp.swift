a5d8fabd76bcfd8d8c8b0d8d78e981f83e59a86b
LeoAndo <reo.ando@sun-asterisk.com>
2022-04-30 15:16:02 +0900

    UIImagePickerControllerのデリゲート処理の実装

---
+++ MyCamera/MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by LeoAndo on 2022/04/30.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
