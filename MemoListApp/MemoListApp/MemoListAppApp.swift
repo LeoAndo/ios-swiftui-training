dab2421d20d9618a003da8e000185ce9155cd1dc
LeoAndo <reo.ando@sun-asterisk.com>
2022-05-10 15:23:31 +0900

    add template project

---
+++ MemoListApp/MemoListApp/MemoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoListAppApp.swift
+//  MemoListApp
+//
+//  Created by LeoAndo on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct MemoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
