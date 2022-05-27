ef93528eb85c46ebda103832b082eab206755bbf
LeoAndo <reo.ando@sun-asterisk.com>
2022-05-27 19:42:41 +0900

    画面作成

---
+++ LocationMonitoring/LocationMonitoring/Model/CLAuthorizationStatus.swift
@@ -0,0 +1,27 @@
+//
+//  CLAuthorizationStatus.swift
+//  LocationMonitoring
+//
+//  Created by LeoAndo on 2022/05/27.
+//
+
+import CoreLocation
+
+extension CLAuthorizationStatus: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case .authorizedAlways:
+      return "Always Authorized"
+    case .authorizedWhenInUse:
+      return "Authorized When In Use"
+    case .denied:
+      return "Denied"
+    case .notDetermined:
+      return "Not Determined"
+    case .restricted:
+      return "Restricted"
+    @unknown default:
+      return "🤷‍♂️"
+    }
+  }
+}
