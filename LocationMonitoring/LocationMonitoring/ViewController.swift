b67d76ffa399065df1c3dd3dcc352201aae00d50
LeoAndo <reo.ando@sun-asterisk.com>
2022-05-27 18:03:32 +0900

    add template project

---
+++ LocationMonitoring/LocationMonitoring/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationMonitoring
+//
+//  Created by LeoAndo on 2022/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
