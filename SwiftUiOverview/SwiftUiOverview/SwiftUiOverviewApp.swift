bd9bc32df028ed7a6b31a1470daa32487deae82e
LeoAndo <reo.ando@sun-asterisk.com>
2022-07-04 14:43:08 +0900

    add template

---
+++ SwiftUiOverview/SwiftUiOverview/SwiftUiOverviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiOverviewApp.swift
+//  SwiftUiOverview
+//
+//  Created by LeoAndo on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiOverviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
