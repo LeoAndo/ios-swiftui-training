f9c13607b5512980baa51542a8984b881d941569
LeoAndo <reo.ando@sun-asterisk.com>
2022-07-04 16:42:02 +0900

    layoutPriority #96

--- SwiftUiOverview/SwiftUiOverview/HStackPlaygroundView.swift
+++ SwiftUiOverview/SwiftUiOverview/HStackPlaygroundView.swift
@@ -20,9 +20,24 @@ struct HStackPlaygroundView: View {
     }
 }
 
+struct HStackPlaygroundView2: View {
+    // layoutPriorityのテスト
+    var body: some View {
+        HStack(alignment: .center, spacing: 20) {
+            Text("First")
+            Image(systemName: "brain.head.profile")
+            Text("SecondText").layoutPriority(1)
+        }
+        .frame(maxWidth: 170)
+        .padding()
+        .background(.green)
+    }
+}
+
 struct HStackPlaygroundView_Previews: PreviewProvider {
     static var previews: some View {
         HStackPlaygroundView()
+        HStackPlaygroundView2()
     }
 }
 
